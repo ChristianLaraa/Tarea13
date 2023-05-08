ade765af80a7bbacc516f7dea29b053a46ff3417
ChristianLaraa <christian.lara.091204@gmail.com>
2023-05-07 20:10:21 -0600

    Tarea 13

---
+++ Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido usuari@");
+
+        //Ventana ventana= new Ventana("Layouts");
+        //VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cuadricula");
+
+        VentanaSwing ventanaSwing= new VentanaSwing("Centigrados a Farenheit");
+    }
+}
